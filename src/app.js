e1f0100b9fe54c2b0c5e7a64867faa276f0fb0f3
Rishabh Agrawal <rishabhagrwl01@gmail.com>
2026-04-06 13:43:59 +0530

    added PATCH and DELETE APIs

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ app.get("/feed", async (req, res) => {
     }
 })
 
+// POST /signup => Create a new user
 app.post('/signup', async (req, res) => {
     // Creating a new instance of the User model
     const user = new User(req.body);
@@ -40,6 +41,31 @@ app.post('/signup', async (req, res) => {
         res.send("User added successfully..!");
     } catch (err) {
         res.status(400).send("Failed to add user..!" + err);
+    }
+});
+
+// DELETE /user => Delete a user by _id
+app.delete("/user", async (req, res) => {
+    const userId = req.body._id;
+    try {
+        // const user = await User.findByIdAndDelete(userId);
+        const user = await User.findByIdAndDelete({ _id: userId });
+        res.send("User deleted successfully..!");
+    } catch (err) {
+        res.status(400).send("Failed to delete user..!" + err);
+    }
+})
+
+// PATCH /user => Update a user by _id
+app.patch("/user", async (req, res) => {
+    const userId = req.body._id;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(userId, data, { returnDocument: "after" });
+        console.log(user);
+        res.send("User updated successfully..!");
+    } catch (err) {
+        res.status(400).send("Failed to update user..!" + err);
     }
 });
 
